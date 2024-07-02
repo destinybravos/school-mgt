9a2eb972d5976b4f1858426e5c52c747121190f3
emmapromax <josiahemmanuel687@gmail.com>
2024-07-02 16:54:39 +0100

    Concluded Register and result page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,46 +1,93 @@
 import React from 'react'
 import Button from '../../components/utils/Button'
+import PrimaryTextinput  from "../../components/utils/PrimaryTextinput"
+
+// import { FaSignInAlt } from "react-icons/fa"
+import { PiBookOpenTextThin } from "react-icons/pi"
+import { Link } from "react-router-dom"
+import { FcGoogle } from "react-icons/fc"
+import { MdAlternateEmail } from "react-icons/md";
+
+import { RiLockPasswordLine } from "react-icons/ri";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 
 const Register = () => {
   return (
     <div>
-       <section >
-          <div className=' min-h-[500px] max-w-[400px] flex flex-col justify-center items-center h-auto px-4 rounded-md'>
-            <h1 className="text-2xl font-sans font-black text-[#334155]">Registration Form</h1>
-            <form action="">
-                <div className="py-4">
-                    <input type="text" name="username" placeholder="Username" required className="w-full h-10 outline-none p-2 bg-transparent text-black border-2 border-primary rounded-md active:border-[#334155] hover:border-[#334155]" />
-                </div>
+       <section className="flex flex-col text-center " >
+        <form>
+        <div className="text-4xl text-primary flex justify-center pt-3 pb-2 ">
+            <div className="py-2 px-2 rounded-full border border-[#0000002f]">
+                <Link to="/" className="" > <PiBookOpenTextThin /></Link>
+            </div>
+            </div>
+            <div>
+                <h1 className="text-3xl font-bold font-sans py-3">Hello Again!</h1>
+                <h6 className="text-sm pb-2">Kindly fill the Required field below to create an account here ok
+                 </h6>
+            </div>
 
-                <div className="py-4">
-                      <input type="text" name="fullname" placeholder="Full Name" required className="w-full h-10 outline-none p-2 bg-transparent text-black border-2 border-primary rounded-md active:border-[#334155] hover:border-[#334155]" />
-                  </div>
-
-                  <div className="py-4">
-                      <input type="email" name="email" placeholder="name@email.com" required className="w-full h-10 outline-none p-2 bg-transparent text-black border-2 border-primary rounded-md active:border-[#334155] hover:border-[#334155]" />
-                  </div >
-
-                  <div className="py-4">
-                      <input type="password" name="password" placeholder="Password" required className="w-full h-10 outline-none p-2 bg-transparent  text-black border-2 border-primary rounded-md active:border-[#334155] hover:border-[#334155]" />
-                  </div>
-
-                  <div className="py-2 relative">
-                      <input type="text"  name="avatar" id="avatar" placeholder="avatar URL" required className="w-full h-10 outline-none p-2 bg-transparent text-black border-2 border-primary rounded-md active:border-[#334155] hover:border-[#334155]" />
-                      <div id="avatar-container" className="absolute max-h-32 overflow-y-auto w-full bg-white ">
-                          <section className="w-full py-1 px-2 grid grid-cols-4"></section>
-                      </div>
-                  </div>
-
-                  <div className="py-4 flex justify-center items-center font-sans text-lg pt-2 pb-1">
-                      <button className="p-2 bg-primary w-[300px] text-white hover:bg-[#334155] ">CREATE ACCOUNT</button>
-                  </div>
-
-                  <div className="flex justify-center items-center font-sans text-lg py-2">
-                      <h1 className="text-primary text-sm hover:underline hover:text-blue-500">Already have an account? Click to Login</h1>
-                  </div>
-            </form>
-          </div>
-       </section>
+            <div className="mb-3">
+                <PrimaryTextinput
+                    icon={<MdOutlineDriveFileRenameOutline className="text-xl"/>}
+                    placeholder="Full Name"
+                    type =""
+                    required
+                />
+            </div>
+            <div className="mb-3">
+                <PrimaryTextinput
+                    icon={<MdAlternateEmail className="text-xl"/>}
+                    placeholder="Enter Email"
+                    type ="Email"
+                    required
+                />
+            </div>
+            <div className="mb-3">
+                <PrimaryTextinput
+                    icon={<MdOutlineDriveFileRenameOutline className="text-xl"/>}
+                    placeholder="Username"
+                    type ="Email"
+                    required
+                />
+            </div>
+            <div className="">
+                <PrimaryTextinput
+                    icon={<RiLockPasswordLine className="text-xl" />}
+                    placeholder="Enter Password"
+                    type="Password"
+                    required
+                />
+            </div>
+            <div className=" my-4">
+                <PrimaryTextinput
+                    icon={<RiLockPasswordLine className="text-xl" />}
+                    placeholder="Confirm Password"
+                    type="Password"
+                    required
+                />
+            </div>
+            
+            <div className="flex gap-1 py-1">
+                <input type="checkbox" name="" id="" className="" required/>
+                <label htmlFor="">I agree to the term of user</label>
+            </div>
+                
+            
+            <div className="pt-5  ">
+            {/* <Button className=" hover:bg-blue-800 w-full border border-[#0000002f]"> Login</Button> */}
+            <Button className="text-white h-11 w-full  justify-center outline-none rounded-md  ">Register</Button>
+            </div>
+            <div className=" bg-white py-2 my-4 w-full border border-[#0000002f] h-11 rounded-md">
+            <Link to="" className="flex justify-center items-center gap-2 hover:text-blue-800"><FcGoogle className="text-3xl" /> Sign up with Google </Link>
+            </div>
+            <div className="mt-10 last:md:mt-14">
+                Already have an account? <Link to="/register" className="text-primary hover:text-blue-800"> Sign in</Link>
+            </div>
+        </form>
+    </section>
+    
+  
 
        
     </div>
